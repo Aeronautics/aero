f4bdd5aeb649580b0f24c613f75fe25551b9e2c0
Jayson Reis <santosdosreis@gmail.com>
2012-07-23 15:08:50 -0300

    added basic search

--- adapters/apt.sh
+++ adapters/apt.sh
@@ -1,6 +1,13 @@
-if [ "$(which apt-get)" ]; then
+APT=$(which apt-get)
+APT_CACHE=$(which apt-cache)
+
+if [ "$APT" ]; then
     export adapter_registered=1
 else
     export adapter_registered=0
 fi
 
+function apt_search {
+    echo "Searching in APT"
+    $APT_CACHE search $1|awk '{print $1}'
+}
--- adapters/brew.sh
+++ adapters/brew.sh
@@ -1,5 +1,12 @@
-if [ "$(which brew)" ]; then
+BREW=$(which brew)
+
+if [ "$BREW" ]; then
     export adapter_registered=1
 else
     export adapter_registered=0
 fi
+
+function brew_search {
+    echo 'Searching in Homebrew'
+    $BREW search $1|awk '{print $1}'
+}
