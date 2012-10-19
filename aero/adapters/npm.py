1a6a07ba969d0a9088b7f77b1fb399ef915745a1
nickl- <github@jigsoft.co.za>
2012-10-19 22:43:30 +0200

    Implement npm search result parcing

--- aero/adapters/npm.py
+++ aero/adapters/npm.py
@@ -4,18 +4,29 @@ from base import BaseAdapter
 from subprocess import Popen
 from subprocess import PIPE
 from string import strip
+from re import match, sub
 class Npm(BaseAdapter):
     adapter_command = 'npm'
 
     def search(self, query):
         response = Popen(self.adapter_command + ' search -q ' + query, shell=True, stdout=PIPE).communicate()[0]
-        lst = list(self.__parse_search(line) for line in response.splitlines() if ' - ' in line)
+        lst = list(self.__parse_search(line) for line in response.splitlines() if 'npm http' not in line and not bool(match('^NAME\s+DESCRIPTION\s+AUTHOR\s+DATE\s+KEYWORDS', line)))
         if lst:
-            return dict(lst)
+            return dict([(k,v) for k,v in lst if k != 0])
         return {}
 
     def __parse_search(self, result):
-        return map(strip, (self.adapter_command + ':' + result).split(' - ', 1))
+        r = match('^([\w-]*)\s+(\w.*)=(.+)\s+(\d\d\d\d[\d\-\: ]*)\s+(\w.*)$', result)
+        if r and len(r.groups()) == 5:
+            r = map(strip, list(r.groups()))
+            pkg = ''.join([self.adapter_command,':', r.pop(0)])
+            r[1] = ' '.join(('Author:', r[1], r[2]))
+            r[2] = r[0]
+            r[3] = 'Tags: ' + ', '.join(r[3].split(' '))
+            del r[0]
+            desc = '\n'.join(r)
+            return (pkg, desc)
+        return (0,0)
 
     def install(self, query):
         print '\n'
