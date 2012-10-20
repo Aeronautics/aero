4877a6e3e0846a6efd92ea88dbe7162e2e0df6e1
nickl- <github@jigsoft.co.za>
2012-10-20 07:15:56 +0200

    Lets quickly add info support for gem
    Think again, what would appear to be an innocent YAML document all
    pretty and human readable turns out to be riddled with constructor tags
    which require classes to map to.
    
    Managed to con the parser that my dictionary stubs are ligit mapping
    classes.
    
    Just before committing why don't we just make sure everything i
    working. 8 hours later....
    
    To throw salt in open wounds some of the gemspecs arrives base64
    encoded with a !binary tag which totally baffles PyYaml and not
    something google has lots to say about either. After having all amounts
    of fun with YAML and the parser I finally discovered that if the tag
    was simply called !!binary everything would've been ok.
    
    Needless to say it workse like a charm now with polymorpic parsing and
    all things nice.

--- aero/adapters/gem.py
+++ aero/adapters/gem.py
@@ -1,16 +1,66 @@
 # -*- coding: utf-8 -*-
 __author__ = 'nickl-'
+from re import sub
 from base import BaseAdapter
-from subprocess import Popen
-from subprocess import PIPE
-from string import strip
+import yaml
+
+class Version(yaml.YAMLObject, dict):
+    yaml_tag = '!ruby/object:Gem::Version'
+    def __setstate__(self,state):
+        self['version'] = state['version']
+
+class Requirement(yaml.YAMLObject, dict):
+    yaml_tag = '!ruby/object:Gem::Requirement'
+    def __setstate__(self,state):
+        r = state['requirements'].pop()
+        self['requirement'] = '{} {}'.format(r.pop(0), r.pop(0)['version'])
+
+class Dependency(yaml.YAMLObject, dict):
+    yaml_tag = '!ruby/object:Gem::Dependency'
+    def __setstate__(self,state):
+        for require in [r for r in state.keys() if 'require' in r]:
+            try :
+                self['requirement'] = state[require]['requirement']
+                break
+            except KeyError: continue
+        self['name'] = state['name']
+        self['type'] = state['type']
+
+class GemSpec(yaml.YAMLObject, dict):
+    yaml_tag = '!ruby/object:Gem::Specification'
+    def __setstate__(self,state):
+        import datetime
+        for k, v in [st for st in state.items() if st[1]]:
+            if isinstance(v, list):
+                if k == 'dependencies':
+                    s = []
+                    mx = 0
+                    for dep in v:
+                        mx = max(len(dep['name']), mx)
+                        s.append((dep['name'], dep['type'], dep['requirement']))
+                    mx += 1
+                    s = '\n'.join(['{:{}}{:12} {:12}'.format(t[0], mx, t[1], t[2]) for t in s])
+                    v = s
+                else:
+                    v = ', '.join(v)
+            if isinstance(v, datetime.datetime):
+                v = '{:%Y-%m-%d}'.format(v)
+            if isinstance(v, Version):
+                v = v['version']
+            if isinstance(v, Requirement):
+                v = v['requirement']
+            if not isinstance(v, str):
+                v = str(v)
+
+            self.update([(k, v)])
+
+
 class Gem(BaseAdapter):
     adapter_command = 'gem'
 
     def search(self, query):
         response = self._execute_command(self.adapter_command, ['search', '-qbd', query])[0]
-        import re
-        rlines = re.sub('\*\*\*.*\*\*\*\s+', '', response).splitlines()
+        rlines = sub('\*\*\*.*\*\*\*\s+', '', response).splitlines()
         lst = {}
         for key, val in [line.split(' ', 1) for line in rlines if line and not line.startswith('  ')]:
             i = rlines.index(key+' '+val) + 1
@@ -28,3 +78,15 @@ class Gem(BaseAdapter):
         print '\n'
         self._execute_shell(self.adapter_command, ['install', query]).wait()
         return {}
+
+    def info(self, query):
+        try:
+            response = self._execute_command(self.adapter_command, ['specification', '-qb', '--yaml', query])[0]
+#            f = open('/Users/inspirex/code/respect/aero/scratch/rubyforge.yaml', 'r')
+#            response = ''.join(f.readlines())
+#            f.close()
+            result = yaml.load(sub(r'!binary', r'!!binary', response))
+            return sorted(result.items())
+
+        except BaseException :
+            return 'Aborted: No info available for a gem called {}'.format(query)
