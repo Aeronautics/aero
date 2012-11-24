469deb0bdad22341701f6ed6b158c2fcc10cd606
nickl- <github@jigsoft.co.za>
2012-11-24 02:30:01 +0200

    Fix setup.py yaml dependency issue #20.
    Because we prepare the adapters when the module is loaded the import yaml dependency comes into play before setup has a chance to install the dependencies.
    Setup retrieves the version information from __version__ which effectively loads the aero module.
    Moved the yaml dependencies to the function call where they are required and therefor does not affect setup.py any longer.
    
    setup.py install
    
    Now collects and installs all the dependencies or fails if this is not possible.
    PyYaml requires lbyaml to be installed or it will fail to build.
    Checking for the presence of PyYaml's dependencies is outside the scope of this project.
    
    This commit resolves #20.

--- aero/adapters/gem.py
+++ aero/adapters/gem.py
@@ -3,73 +3,9 @@ __author__ = 'nickl-'
 __all__ = ('Gem', )
 
 from re import sub
-import yaml
 
 from .base import BaseAdapter
 from aero.__version__ import __version__
-
-
-class Version(yaml.YAMLObject, dict):
-
-    yaml_tag = '!ruby/object:Gem::Version'
-
-    def __setstate__(self, state):
-        self['version'] = state['version']
-
-
-class Requirement(yaml.YAMLObject, dict):
-
-    yaml_tag = '!ruby/object:Gem::Requirement'
-
-    def __setstate__(self, state):
-        r = state['requirements'].pop()
-        self['requirement'] = '{} {}'.format(r.pop(0), r.pop(0)['version'])
-
-
-class Dependency(yaml.YAMLObject, dict):
-
-    yaml_tag = '!ruby/object:Gem::Dependency'
-
-    def __setstate__(self, state):
-        for require in [r for r in state.keys() if 'require' in r]:
-            try:
-                self['requirement'] = state[require]['requirement']
-                break
-            except KeyError:
-                continue
-        self['name'] = state['name']
-        self['type'] = state['type']
-
-
-class GemSpec(yaml.YAMLObject, dict):
-
-    yaml_tag = '!ruby/object:Gem::Specification'
-
-    def __setstate__(self, state):
-        import datetime
-        for k, v in [st for st in state.items() if st[1]]:
-            if isinstance(v, list):
-                if k == 'dependencies':
-                    s = []
-                    mx = 0
-                    for dep in v:
-                        mx = max(len(dep['name']), mx)
-                        s.append((dep['name'], dep['type'], dep['requirement']))
-                    mx += 1
-                    s = '\n'.join(['{:{}}{:12} {:12}'.format(t[0], mx, t[1], t[2]) for t in s])
-                    v = s
-                else:
-                    v = ', '.join(v)
-            if isinstance(v, datetime.datetime):
-                v = '{:%Y-%m-%d}'.format(v)
-            if isinstance(v, Version):
-                v = v['version']
-            if isinstance(v, Requirement):
-                v = v['requirement']
-            if not isinstance(v, str):
-                v = str(v)
-
-            self.update([(k, v)])
 
 
 class Gem(BaseAdapter):
@@ -102,6 +38,71 @@ class Gem(BaseAdapter):
         return {}
 
     def info(self, query):
+
+        import yaml
+
+        class Version(yaml.YAMLObject, dict):
+
+            yaml_tag = '!ruby/object:Gem::Version'
+
+            def __setstate__(self, state):
+                self['version'] = state['version']
+
+
+        class Requirement(yaml.YAMLObject, dict):
+
+            yaml_tag = '!ruby/object:Gem::Requirement'
+
+            def __setstate__(self, state):
+                r = state['requirements'].pop()
+                self['requirement'] = '{} {}'.format(r.pop(0), r.pop(0)['version'])
+
+
+        class Dependency(yaml.YAMLObject, dict):
+
+            yaml_tag = '!ruby/object:Gem::Dependency'
+
+            def __setstate__(self, state):
+                for require in [r for r in state.keys() if 'require' in r]:
+                    try:
+                        self['requirement'] = state[require]['requirement']
+                        break
+                    except KeyError:
+                        continue
+                self['name'] = state['name']
+                self['type'] = state['type']
+
+
+        class GemSpec(yaml.YAMLObject, dict):
+
+            yaml_tag = '!ruby/object:Gem::Specification'
+
+            def __setstate__(self, state):
+                import datetime
+                for k, v in [st for st in state.items() if st[1]]:
+                    if isinstance(v, list):
+                        if k == 'dependencies':
+                            s = []
+                            mx = 0
+                            for dep in v:
+                                mx = max(len(dep['name']), mx)
+                                s.append((dep['name'], dep['type'], dep['requirement']))
+                            mx += 1
+                            s = '\n'.join(['{:{}}{:12} {:12}'.format(t[0], mx, t[1], t[2]) for t in s])
+                            v = s
+                        else:
+                            v = ', '.join(v)
+                    if isinstance(v, datetime.datetime):
+                        v = '{:%Y-%m-%d}'.format(v)
+                    if isinstance(v, Version):
+                        v = v['version']
+                    if isinstance(v, Requirement):
+                        v = v['requirement']
+                    if not isinstance(v, str):
+                        v = str(v)
+
+                    self.update([(k, v)])
+
         try:
             response = self._execute_command(
                 self.adapter_command, ['specification', '-qb', '--yaml', query]
