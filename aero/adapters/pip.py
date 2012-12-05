64078daac7791af4061bc1de7913d8a76254a4c1
nickl- <github@jigsoft.co.za>
2012-12-05 07:09:48 +0200

    pip: Rewrite search to use api
    Instead of external process us pip library instead

--- aero/adapters/pip.py
+++ aero/adapters/pip.py
@@ -12,17 +12,16 @@ class Pip(BaseAdapter):
     Pip adapter.
     """
     def search(self, query):
-        response = self.command(['search', query])[0]
+        m = import_module('pip.commands.search')
         lst = {}
-        from re import match
-        for key, line in [map(
-            strip, self.package_name(l).split(' - ', 1)
-        ) for l in response.splitlines() if ' - ' in l]:
-            parts = match('(.*)[ <\\(]?(http.*?)?[ >\\)]?(.*)', line).groups()
-            lst[key] = parts[0] + ' ' + parts[2] + ('\n' + parts[1] if parts[1] else '')
-        if lst:
-            return lst
-        return {}
+        for r in m.transform_hits(m.SearchCommand().search(query, 'http://pypi.python.org/pypi')):
+            summary = ' '.join(map(strip, r['summary'].split('\n'))).replace('  ', ' ')
+            lst[self.package_name(r['name'])] = 'Version: {:<14} Score:{:>4}\n{}'.format(
+                max(r['versions']),
+                r['score'],
+                (summary if len(summary) < 200 else summary[:190] + '...').replace('  ', ' ')
+            )
+        return lst
 
     def install(self, query):
         self.shell([
