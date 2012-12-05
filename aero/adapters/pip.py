4f2952326ed0dcec817a9b0ae130790299e0799e
nickl- <github@jigsoft.co.za>
2012-12-05 07:23:57 +0200

    pip still cater for pass through arguments

--- aero/adapters/pip.py
+++ aero/adapters/pip.py
@@ -24,16 +24,13 @@ class Pip(BaseAdapter):
         return lst
 
     def install(self, query):
-        import_module('pip').main(
-            [
-                'install',
-                '--force-reinstall',
-                '--upgrade',
-                '--timeout', '30',
-                '--egg',
-                '--log', '~/.aero/log/pip.log',
-                '--download-cache', '~/.aero/cache/pip',
-                query,
-            ]
-        )
+        import_module('pip').main([
+            'install',
+            '--force-reinstall',
+            '--upgrade',
+            '--timeout', '30',
+            '--egg',
+            '--log', '~/.aero/log/pip.log',
+            '--download-cache', '~/.aero/cache/pip',
+        ] + self.passthru + [query])
         return {}
